0a854a407e38171af8b4ce80fb0eaa9331f908b4
stepyen <stepyen@163.com>
2019-07-01 10:01:02 +0800

    修改依赖

--- testjitpack/src/main/java/com/stepyen/testjitpack/Test.java
+++ testjitpack/src/main/java/com/stepyen/testjitpack/Test.java
@@ -1,12 +1,18 @@
 package com.stepyen.testjitpack;
 
+import android.app.Activity;
+import android.content.Context;
+
+import com.alipay.sdk.app.PayTask;
+
 /**
  * date：2019/7/1
  * author：stepyen
  * description：
  */
 public class Test {
-    public static String test() {
-        return "test";
+    public static String test(Activity context) {
+        PayTask alipay = new PayTask(context);
+        return alipay.getVersion().toString();
     }
 }
