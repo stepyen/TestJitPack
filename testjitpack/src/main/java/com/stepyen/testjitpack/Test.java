077975d1f23ca32d1ce4ab968ea5799fe1aaf6ef
stepyen <stepyen@163.com>
2019-07-01 09:15:10 +0800

    Initial commit

---
+++ testjitpack/src/main/java/com/stepyen/testjitpack/Test.java
@@ -0,0 +1,12 @@
+package com.stepyen.testjitpack;
+
+/**
+ * date：2019/7/1
+ * author：stepyen
+ * description：
+ */
+public class Test {
+    public static String test() {
+        return "test";
+    }
+}
